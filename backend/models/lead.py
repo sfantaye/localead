d72cb662aeca2eb572d1e68c7fbf13589c020e89
Sintayehu G. Fantaye <131191039+sfantaye@users.noreply.github.com>
2025-04-20 12:40:59 -0700

    Update lead.py

--- backend/models/lead.py
+++ backend/models/lead.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 
 class Lead(BaseModel):
     name: str
-    address: str
+    address: str | None = None
     phone: str | None = None
     email: str | None = None
     website: str | None = None
